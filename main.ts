a65325c6cee648eed9a19906a356a9dd2c12c518
bobthegreif1 <80830961+bobthegreif1@users.noreply.github.com>
2021-03-23 17:22:52 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     info.changeScoreBy(1)
    task()
+   donut.setPosition(randint(0, 160),randint(0,120))
 })
 let mypizza: Sprite = null
 scene.setBackgroundColor(89)
@@ -81,3 +82,23 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 info.changeScoreBy(-5)
 posionfood.setPosition(randint(0, 160),randint(0,120))
 })
+
+let donut = sprites.create(img`
+    . . . . . . b b b b a a . . . .
+    . . . . b b d d d 3 3 3 a a . .
+    . . . b d d d 3 3 3 3 3 3 a a .
+    . . b d d 3 3 3 3 3 3 3 3 3 a .
+    . b 3 d 3 3 3 3 3 b 3 3 3 3 a b
+    . b 3 3 3 3 3 a a 3 3 3 3 3 a b
+    b 3 3 3 3 3 a a 3 3 3 3 d a 4 b
+    b 3 3 3 3 b a 3 3 3 3 3 d a 4 b
+    b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e
+    a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e
+    a 3 3 3 3 3 3 3 d d a 4 4 4 e .
+    a a 3 3 3 d d d a a 4 4 4 e e .
+    . e a a a a a a 4 4 4 4 e e . .
+    . . e e b b 4 4 4 4 b e e . . .
+    . . . e e e e e e e e . . . . .
+    . . . . . . . . . . . . . . . .
+`,SpriteKind.Food)
+donut.setPosition(randint(0, 160),randint(0,120))
